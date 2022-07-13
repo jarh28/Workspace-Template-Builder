38fa7635c4d724c4cad231b5b7aada7a915445f6
jarh-desktop <jacintorivero28@gmail.com>
2022-07-12 21:29:15 -0400

    Added components and responsive behavior

---
+++ src/WorkspacePreview.js
@@ -0,0 +1,9 @@
+function WorkspacePreview() {
+    return (
+        <div className="workspace-preview-component">
+            <h1>WorkspacePreview</h1>
+        </div>
+    );
+}
+
+export default WorkspacePreview;
---
+++ src/WorkspaceSettings.js
@@ -0,0 +1,9 @@
+function WorkspaceSettings() {
+    return (
+        <div className="workspace-settings-component">
+            <h1>WorkspaceSettings</h1>
+        </div>
+    );
+}
+
+export default WorkspaceSettings;
